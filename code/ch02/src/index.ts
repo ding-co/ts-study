575832aeda2db27e35ebcf595d663879d62fe971
ding-co <pangoons@naver.com>
2022-02-03 13:05:23 +0900

    [ADD] TS ch 02 - TS look down

---
+++ code/ch02/src/index.ts
@@ -0,0 +1,13 @@
+// console.log('Hello TypeScript!');
+
+let a = 1 + 2; // 3
+let b = a + 3; // 6
+let c = {
+  apple: a,
+  banana: b,
+};
+// {
+//   apple: 3,
+//   banana: 6
+// }
+let d = c.apple * 4; // 12
